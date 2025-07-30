0b8b46ea8c36520482fe4dc015450326343f46ee
Cesar Navarro <cnfrs9@gmail.com>
2025-07-29 21:49:28 -0300

    Creating Post entity with nested User

--- src/main/java/com/cesarnavarro/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/cesarnavarro/workshopmongodb/config/Instantiation.java
@@ -1,30 +1,46 @@
 package com.cesarnavarro.workshopmongodb.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.cesarnavarro.workshopmongodb.domain.Post;
 import com.cesarnavarro.workshopmongodb.domain.User;
+import com.cesarnavarro.workshopmongodb.repository.PostRepository;
 import com.cesarnavarro.workshopmongodb.repository.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
 	@Autowired
-	UserRepository userRepository;
+	private UserRepository userReposiroty;
+
+	@Autowired
+	private PostRepository postReposiroty;
 
 	@Override
-	public void run(String... args) throws Exception {
+	public void run(String... arg0) throws Exception {
 
-		userRepository.deleteAll();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+		userReposiroty.deleteAll();
+		postReposiroty.deleteAll();
 
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "I'm traveling tomorrow",
+				"I'm going to travel to São Paulo!", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Good morning", "I woke up happy today!", maria);
+
+		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+		postReposiroty.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
