85a8dd244ed2729981d9f1516c2899d5a00b6491
Cesar Navarro <cnfrs9@gmail.com>
2025-07-30 21:06:12 -0300

    Getting a post by id

---
+++ src/main/java/com/cesarnavarro/workshopmongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.cesarnavarro.workshopmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cesarnavarro.workshopmongodb.domain.Post;
+import com.cesarnavarro.workshopmongodb.repository.PostRepository;
+import com.cesarnavarro.workshopmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
